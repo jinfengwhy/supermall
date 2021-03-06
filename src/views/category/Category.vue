f784791a7f121cb950936a03b1978f24f5e7f052
wjinfeng <xingzhe290@foxmail.com>
2021-03-06 12:20:27 +0800

    p167 Better-Scroll在Vue项目中使用过程

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -115,7 +115,25 @@
       }
     },
     mounted() {
+      // 默认情况下BScroll是不可以实时的监听滚动位置
+      // probe 侦测
+      // 0,1都是不侦测实时的位置
+      // 2: 在手指滚动的过程中侦测, 手指离开后的惯性滚动过程中不侦测.
+      // 3: 只要是滚动, 都侦测.
       this.scroll = new BScroll(document.querySelector('.wrapper'), {
+        probeType: 3,
+        pullUpLoad: true
+      })
+
+      this.scroll.on('scroll', position => {
+        console.log(position);
+      })
+
+      this.scroll.on('pullingUp', () => {
+        console.log('上拉加载更多');
+        setTimeout(() => {
+          this.scroll.finishPullUp()
+        }, 2000)
       })
     }    
   }
