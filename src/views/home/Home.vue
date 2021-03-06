3b0e2c83b5ac691706e055d6805ca7d5f73755fb
wjinfeng <xingzhe290@foxmail.com>
2021-03-06 15:41:31 +0800

    p171 首页开发 - 完成上拉加载更多

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,7 +5,9 @@
       <div slot="center">购物街</div>
     </nav-bar>
 
-    <scroll class="content" ref="scroll" :probeType="3" @scroll="contentScroll">
+    <scroll class="content" ref="scroll" 
+      :probe-type="3" @scroll="contentScroll"
+      :pull-up-load="true" @pullingUp="loadMore">
       <home-swiper :banners="banners"/>
       <recommend-view :recommends="recommends"/>
       <feature-view/>
@@ -94,6 +96,10 @@
       contentScroll(position) {
         this.isShowBackTop = (-position.y) > 1000
       },
+      loadMore() {
+        console.log('触发了上拉加载更多');
+        this.getHomeGoods(this.currentType)
+      },
 
       /**
        * 网络请求相关的方法
@@ -109,6 +115,8 @@
         getHomeGoods(type, page).then(res => {
           this.goods[type].list.push(...res.data.list)
           this.goods[type].page += 1
+          
+          this.$refs.scroll.finishPullUp()
         })
       }
     }
