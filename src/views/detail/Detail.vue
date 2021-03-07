a707c6106223a57487e3294d675db9798124793d
wjinfeng <xingzhe290@foxmail.com>
2021-03-07 10:45:43 +0800

    p180 跳转到详情页并且携带iid

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    我是详情页{{iid}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      iid: null
+    }
+  },
+  created() {
+    this.iid = this.$route.query.iid
+  }
+}
+</script>
