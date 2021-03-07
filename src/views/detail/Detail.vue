7e25e4f5c8df88f05e7e7d34d1afab994c367b60
wjinfeng <xingzhe290@foxmail.com>
2021-03-07 11:59:11 +0800

    p182 详情页 - 数据请求以及轮播图展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="detail">
     <detail-nav-bar/>
+    <detail-swiper :top-images="topImages"/>
   </div>
 </template>
 
 <script>
+import {getDetail} from 'network/detail'
 import DetailNavBar from './childComps/DetailNavBar'
+import DetailSwiper from './childComps/DetailSwiper'
 
 export default {
+  name: 'Detail',
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: []
     }
   },
   components: {
-    DetailNavBar
+    DetailNavBar,
+    DetailSwiper
   },
   created() {
+    // 1.保存传过来的商品id
     this.iid = this.$route.query.iid
+
+    // 2.请求详情页所需的数据
+    getDetail(this.iid).then(res => {
+      console.log(res);
+      this.topImages = res.result.itemInfo.topImages
+    })
   }
 }
 </script>
