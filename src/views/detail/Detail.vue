c4c2710952845d4c9b94914d1d2867ad379a2d95
wjinfeng <xingzhe290@foxmail.com>
2021-03-07 14:24:59 +0800

    p183 详情页 - 商品基本信息的展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,25 +2,29 @@
   <div id="detail">
     <detail-nav-bar/>
     <detail-swiper :top-images="topImages"/>
+    <detail-base-info :goods="goods"/>
   </div>
 </template>
 
 <script>
-import {getDetail} from 'network/detail'
+import {getDetail, Goods} from 'network/detail'
 import DetailNavBar from './childComps/DetailNavBar'
 import DetailSwiper from './childComps/DetailSwiper'
+import DetailBaseInfo from './childComps/DetailBaseInfo'
 
 export default {
   name: 'Detail',
   data() {
     return {
       iid: null,
-      topImages: []
+      topImages: [],
+      goods: {}
     }
   },
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo
   },
   created() {
     // 1.保存传过来的商品id
@@ -29,7 +33,9 @@ export default {
     // 2.请求详情页所需的数据
     getDetail(this.iid).then(res => {
       console.log(res);
-      this.topImages = res.result.itemInfo.topImages
+      const data = res.result
+      this.topImages = data.itemInfo.topImages
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
     })
   }
 }
